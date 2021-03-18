d5e60d9044601e1aed6de6c34d2041cd4f1bd0a6
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2021-03-18 19:36:09 +0000

    added industry allocations and total value

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,7 +1,8 @@
 from data import DataLoader, OrderParser
 from assets.fundamentals import AssetFundamentals
+from assets.pricer import Pricer
 from helpers import DateFormatter, DateHelper
-from constants import DateFormat, OrderColumn, StockFundamentals
+from constants import DateFormat, OrderColumn, StockFundamentals, Industry
 import datetime
 
 class Portfolio:
@@ -69,9 +70,53 @@ class Portfolio:
 
         Returns
         -------
-        dict: containing positions {stock: amount}
+        dict: containing fundamentals {fundamental: list of stocks}
         '''
         try:
             return AssetFundamentals.get_stock_fundamental(fundamental, self.get_stocks(date))
         except Exception as e:
             raise Exception(e)
+
+    def get_industry_allocations(self, date=None):
+        '''
+        position weight = total value of position / total value of portfolio
+        '''
+        # Categorise stocks by industry
+        industry_dict = self.get_fundamentals(StockFundamentals.industry, date=date)
+        # positions_dict = self.get_positions(date)
+        # prices_dict = Pricer.get_current_price(positions_dict)
+        self.total_val = self.get_total_value(date)
+        res = {}
+        for industry in industry_dict:
+            stocks = industry_dict[industry]
+            val = self.get_value(stocks, self.positions_dict, self.prices_dict)
+            pct = val*100/self.total_val
+            res[industry] = pct
+        return res
+
+    def get_value(self, stocks, positions, prices):
+        '''
+        Gets the total value of a list of stocks given a dictionary of positions for a portfolio
+        '''
+        val = 0
+        for stock in stocks:
+            val += positions[stock]*prices[stock]
+        return val
+
+    def get_stocks_in_industry(self, industry_data=None, industry: str=None, date=None):
+        if not industry_data:
+            industry_data = self.get_fundamentals(StockFundamentals.industry, date)
+        return industry_data.get(industry)
+
+
+    def get_total_value(self, date=None):
+        '''
+        Gets the total value of the portfolio.
+        Note: Stocks that aren't supported by yfinance API won't be accounted for
+        '''
+        self.positions_dict = self.get_positions(date)
+        self.prices_dict = Pricer.get_current_price(self.positions_dict)
+        val = 0
+        for stock in self.prices_dict:
+            val += self.positions_dict[stock]*self.prices_dict[stock]
+        return val
