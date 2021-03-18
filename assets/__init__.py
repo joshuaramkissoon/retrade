cb7af5c466693526de40c1d69420245668092dd9
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2021-03-18 19:34:59 +0000

    changed imports

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,4 +1,2 @@
 from assets.fundamentals import AssetFundamentals
 from assets.pricer import Pricer
-
-Pricer = Pricer()
--- assets/pricer/__init__.py
+++ assets/pricer/__init__.py
@@ -1,1 +1,3 @@
 from assets.pricer.pricer import Pricer
+
+Pricer = Pricer()
