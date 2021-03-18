f2f04a4efeb467aa087ce3691b2fa0029a579870
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2021-03-18 22:15:56 +0000

    updated run code

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
-from data import DataLoader, OrderParser
 from analytics import ReturnsCalculator
-from constants import OrderColumn
+from data import DataLoader, OrderParser
+from constants import OrderColumn, StockFundamentals, PortfolioFilter
 import datetime
 
-path = 'order history Nov_Mar17.csv'
-cols = [OrderColumn.date, OrderColumn.action, OrderColumn.ticker, OrderColumn.num_shares]
-parser = OrderParser(DataLoader.load_order_csv(path, cols))
-pf = parser.get_portfolio_on_date(datetime.datetime.now() + datetime.timedelta(days=-131))
-print(ReturnsCalculator.get_daily_change(pf, datetime.datetime.now() + datetime.timedelta(days=-131)))
+
+path = 'order history Nov_Mar18.csv'
+# cols = [OrderColumn.date, OrderColumn.action, OrderColumn.ticker, OrderColumn.num_shares]
+# parser = OrderParser(DataLoader.load_order_csv(path, cols))
+# d = parser.get_portfolio_on_date(datetime.datetime.now())
+# print(ReturnsCalculator.get_daily_change(pf, datetime.datetime.now() + datetime.timedelta(days=-132)))
+
+
+from portfolio import Portfolio
+from visualisations.visualisations import plot_allocations
+
+pf = Portfolio(path=path)
+res = pf.get_group_allocations(PortfolioFilter.industry, include_stocks=False)
+plot_allocations(PortfolioFilter.industry, res)
+
+
