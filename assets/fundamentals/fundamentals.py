f096b0eccb687837d627fe722a9949b22c6bd94e
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2021-03-30 21:13:41 +0100

    refactored and added get stocks by group method

--- assets/fundamentals/fundamentals.py
+++ assets/fundamentals/fundamentals.py
@@ -1,9 +1,9 @@
 import yfinance as yf
-from constants import StockFundamentals
+from constants import StockFundamentals, StockGrouping
 
 class AssetFundamentals:
     
-    def get_fundamentals(stock_ticker):
+    def get_all_fundamentals(stock_ticker):
         ticker = yf.Ticker(stock_ticker)
         try:
             info = ticker.info
@@ -13,35 +13,70 @@ class AssetFundamentals:
 
     def get_industry(stock_ticker):
         try:
-            info = AssetFundamentals.get_fundamentals(stock_ticker)
+            info = AssetFundamentals.get_all_fundamentals(stock_ticker)
             return info.get('industry')
         except Exception as e:
             raise Exception(e)
 
-    def get_stock_fundamental(fundamental, stocks):
+    def get_stocks_by_group(group: StockGrouping, stocks):
+        '''
+        Groups a list of stocks by certain grouping criteria.
+        Parameters
+        ----------
+        group (StockGrouping enum) - how to group stocks
+        stocks - list of stock tickers (str)
+
+        Returns
+        -------
+        dict - Dictionary {group: [stocks]}
+        '''
         s = ' '.join(stocks)
         tickers = yf.Tickers(s)
-        res = {} # {Fundamental: [Stocks]}
+        res = {} # {Group: [Stocks]}
         for ticker in tickers.tickers:
             try:
-                asset_class = ticker.info.get('quoteType')
-                if asset_class != 'EQUITY':
+                if (asset_class := ticker.info.get('quoteType')) != 'EQUITY':
                     print('%s is not an equity asset, it is a %s.' % (ticker.ticker, asset_class))
                     continue
-                fnd = ticker.info.get(fundamental.value)
-                if fnd not in res:
-                    res[fnd] = [ticker.ticker]
+                if (grp := ticker.info.get(group.value)) not in res:
+                    res[grp] = [ticker.ticker]
                 else:
-                    res[fnd].append(ticker.ticker)
+                    res[grp].append(ticker.ticker)
             except:
-                print('No fundamentals for ticker: ', ticker.ticker)
-                continue
-        return res      
+                print('No grouping for ticker: ', ticker.ticker)
+        return res    
+
+    def get_fundamental(fundamental: StockFundamentals, stocks):
+        '''
+        Gets specified fundamental for a list of stocks.
+        Parameters
+        ----------
+        fundamental (StockFundamental) - fundamental to get for each stock
+        stocks (list of str) - stock tickers to get fundamentals for
+
+        Returns
+        -------
+        dict - {stock: fundamental}
+        '''
+        s = ' '.join(stocks)
+        tickers = yf.Tickers(s)
+        res = {}
+        for ticker in tickers.tickers:
+            try:
+                if fnd := ticker.info.get(fundamental.value):
+                    if ticker not in res:
+                        res[ticker.ticker] = fnd
+                else:
+                    print('No %s for ticker: %s ' % (fundamental.value, ticker.ticker))    
+                    continue
+            except:
+                print('No %s for ticker: %s ' % (fundamental.value, ticker.ticker))
+        return res
 
 
     def get_sector(stock_ticker):
         try:
-            info = AssetFundamentals.get_fundamentals(stock_ticker)
+            info = AssetFundamentals.get_all_fundamentals(stock_ticker)
             return info.get('sector')
         except Exception as e:
             raise Exception(e)
